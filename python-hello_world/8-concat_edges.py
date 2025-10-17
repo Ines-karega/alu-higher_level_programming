1cad51b283f58966269c347c232e28be4a52b1db
Ines-karega <i.karega@alustudent.com>
2025-10-17 04:11:40 -0700

    nbr 8

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-word1 = "object"
-word2 = "oriented"
-word3 = "programming"
-word4 = "with"
-word5 = "Python"
-print(word1 + "-" + word2 + " " + word3 + " " + word4 + " " + word5)
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+print(str[39:67] + str[107:112] + str[0:6])
